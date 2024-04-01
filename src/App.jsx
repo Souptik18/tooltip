1d358ad480511d5c555f048c77de12b50bbc1496
Souptik18 <62500243+Souptik18@users.noreply.github.com>
2024-04-01 14:12:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Tooltip from "./components/Tooltip";
 
+//here tooltip component is taking position as a prop  
+//added extra styling to the button 
 const App = () => {
   return (
     <div style={{ textAlign: "center", marginTop: "100px" }}>
